aac76109f8ca614747450ff8df2958ef239ec8ea
PACHABOTLALAKSHMISRINIDH <plakshmisrinidh99@gmail.com>
2022-04-24 01:47:07 +0200

    return value 0 will fail the test case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -90,7 +90,7 @@ public class Main {
 
             }
         }
-        return count;
+        return 0;
     }
     //method 2
     //description: count the Spaces in the given string
@@ -115,6 +115,6 @@ public class Main {
                     count=count+1;
                 }
             }
-            return count;
+            return 0;
         }
     }
